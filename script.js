a7e6123ddea3f16f786bde29257de13efe8290b8
Kevin Herrera <1herrerake@gmail.com>
2017-01-31 12:37:28 -0500

    initial js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("hello world");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+let green = document.getElementById("green")
+let red = document.getElementById("red")
+let yellow = document.getElementById("yellow")
+let blue = document.getElementById("blue")
+
+function clickDom(){
+  this.className += " animation";
+}
+
+green.addEventListener("click", clickDom)
+})
